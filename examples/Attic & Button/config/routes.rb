e7e8253acde7b6248a65525313b64b7b9b5101c4
rashidsp <rashidsp204@gmail.com>
2018-01-18 22:12:50 +0500

    rubcop routes

--- examples/Attic & Button/config/routes.rb
+++ examples/Attic & Button/config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 #    Copyright 2018, Optimizely and contributors
 #
@@ -15,22 +17,27 @@
 #
 
 Rails.application.routes.draw do
-  resources :demo, only: [:new, :create]
+  resources :demo, only: %i[new create]
+
+  # Binding user_id auth requests
+  post 'demo/:user_id/buy', to: 'demo#buy', as: :buy
   get 'demo/:user_id/cart', to: 'demo#cart', as: :cart
   get 'demo/:user_id/checkout_cart', to: 'demo#checkout_cart', as: :checkout_cart
-  post 'demo/:user_id/buy', to: 'demo#buy', as: :buy
-  get 'demo/login', to: 'demo#new'
-  post 'demo/config', to: 'demo#create'
-  post  'demo/:user_id/checkout_payment', to: 'demo#checkout_payment', as: :checkout_payment
+  post 'demo/:user_id/checkout_payment', to: 'demo#checkout_payment', as: :checkout_payment
   delete 'demo/:user_id/delete_messages', to: 'demo#delete_messages', as: :delete_messages
   delete 'demo/:user_id/delete_cart', to: 'demo#delete_cart', as: :delete_cart
-  delete 'demo/logout', to: 'demo#logout', as: :logout
   get 'demo/:user_id/messages', to: 'demo#log_messages', as: :messages
-  get "/demo/shop" => "demo#guest_shop", :as => :guest_shop
-  get "/demo/:user_id/shop" => "demo#shop", :as => :shop
-  get "/demo/:user_id/payment" => "demo#payment", :as => :payment
-  put "/demo/:user_id/update_cart" => "demo#update_cart", :as => :update_cart
-  get "/show_receipt/:user_id/show_receipt" => "demo#show_receipt", :as => :show_receipt
+  get '/demo/:user_id/payment' => 'demo#payment', as: :payment
+  get '/demo/:user_id/shop' => 'demo#shop', as: :shop
+  put '/demo/:user_id/update_cart' => 'demo#update_cart', as: :update_cart
+  get '/show_receipt/:user_id/show_receipt' => 'demo#show_receipt', as: :show_receipt
+
+  # Requests before user logged in
+  post 'demo/config', to: 'demo#create'
+  get 'demo/login', to: 'demo#new'
+  delete 'demo/logout', to: 'demo#logout', as: :logout
+  get '/demo/shop' => 'demo#guest_shop', as: :guest_shop
+
   # root path
   root 'demo#guest_shop'
 end
