50179e53b9fa3e42c25e810f475011ce10cffeba
Rashid Siddique <rashidsp204@gmail.com>
2018-12-17 22:53:58 +0500

    audience combination tests

--- spec/audience_spec.rb
+++ spec/audience_spec.rb
@@ -21,10 +21,12 @@ describe Optimizely::Audience do
   before(:context) do
     @config_body = OptimizelySpec::VALID_CONFIG_BODY
     @config_body_json = OptimizelySpec::VALID_CONFIG_BODY_JSON
+    @config_typed_audience_json = JSON.dump(OptimizelySpec::CONFIG_DICT_WITH_TYPED_AUDIENCES)
   end
 
   before(:example) do
     @project_instance = Optimizely::Project.new(@config_body_json)
+    @project_typed_audience_instance = Optimizely::Project.new(@config_typed_audience_json)
   end
 
   it 'should return true for user_in_experiment? when experiment is using no audience' do
@@ -123,29 +125,71 @@ describe Optimizely::Audience do
                                                     user_attributes)).to be false
   end
 
-  # it 'should correctly evaluate audience Ids and calls custom attribute evaluator for leaf nodes' do
-  #   experiment = @project_instance.config.experiment_key_map['test_experiment_with_audience']
-  #   user_attributes = {
-  #     'browser_type' => 'firefox'
-  #   }
-  #   experiment['audienceIds'] = ['11154', '11159']
-  #   experiment['audienceConditions'] = nil
-  #
-  #   audience_11154 = @project_instance.config.get_audience_from_id('11154')
-  #   audience_11159 = @project_instance.config.get_audience_from_id('11159')
-  #
-  #   customer_attr = Optimizely::CustomAttributeConditionEvaluator.new({})
-  #   allow(customer_attr.evaluate(audience_11154['conditions'])).to receive(:send)
-  #
-  #   Optimizely::Audience.user_in_experiment?(@project_instance.config,experiment,{})
-  #
-  #   # expect(dbl).to have_received(:one).ordered
-  #   # expect(dbl).to have_received(:two).ordered
-  #
-  #   expect(customer_attr.evaluate(audience_11154['conditions']))
-  #     .to have_received(:send).with(:exact_evaluator, audience_11154['conditions']).once
-  #
-  #
-  #
-  # end
+  it 'should correctly evaluate audience Ids and calls custom attribute evaluator for leaf nodes' do
+    experiment = @project_instance.config.experiment_key_map['test_experiment_with_audience']
+    user_attributes = {
+      'browser_type' => 'firefox'
+    }
+    experiment['audienceIds'] = %w[11154 11155]
+    experiment['audienceConditions'] = nil
+
+    audience_11154 = @project_instance.config.get_audience_from_id('11154')
+    audience_11155 = @project_instance.config.get_audience_from_id('11155')
+    audience_11154_condition = JSON.parse(audience_11154['conditions'])[1][1][1]
+    audience_11155_condition = JSON.parse(audience_11155['conditions'])[1][1][1]
+
+    customer_attr = Optimizely::CustomAttributeConditionEvaluator.new(user_attributes)
+    allow(customer_attr).to receive(:exact_evaluator)
+    customer_attr.evaluate(audience_11154_condition)
+    customer_attr.evaluate(audience_11155_condition)
+
+    expect(customer_attr).to have_received(:exact_evaluator).with(audience_11154_condition).once
+    expect(customer_attr).to have_received(:exact_evaluator).with(audience_11155_condition).once
+  end
+
+  it 'should correctly evaluates audienceConditions and calls custom attribute evaluator for leaf nodes' do
+    experiment = @project_typed_audience_instance.config.get_experiment_from_key('audience_combinations_experiment')
+    experiment['audienceIds'] = []
+    experiment['audienceConditions'] = ['or', %w[or 3468206642 3988293898], %w[or 3988293899 3468206646]]
+
+    audience_3468206642 = @project_typed_audience_instance.config.get_audience_from_id('3468206642')
+    audience_3988293898 = @project_typed_audience_instance.config.get_audience_from_id('3988293898')
+    audience_3988293899 = @project_typed_audience_instance.config.get_audience_from_id('3988293899')
+    audience_3468206646 = @project_typed_audience_instance.config.get_audience_from_id('3468206646')
+
+    audience_3468206642_condition = JSON.parse(audience_3468206642['conditions'])[1][1][1]
+    audience_3988293898_condition = audience_3988293898['conditions'][1][1][1]
+    audience_3988293899_condition = audience_3988293899['conditions'][1][1][1]
+    audience_3468206646_condition = audience_3468206646['conditions'][1][1][1]
+
+    customer_attr = Optimizely::CustomAttributeConditionEvaluator.new({})
+    allow(customer_attr).to receive(:exact_evaluator)
+    allow(customer_attr).to receive(:substring_evaluator)
+    allow(customer_attr).to receive(:exists_evaluator)
+    customer_attr.evaluate(audience_3468206642_condition)
+    customer_attr.evaluate(audience_3988293898_condition)
+    customer_attr.evaluate(audience_3988293899_condition)
+    customer_attr.evaluate(audience_3468206646_condition)
+
+    expect(customer_attr).to have_received(:exact_evaluator).with(audience_3468206642_condition).once
+    expect(customer_attr).to have_received(:substring_evaluator).with(audience_3988293898_condition).once
+    expect(customer_attr).to have_received(:exists_evaluator).with(audience_3988293899_condition).once
+    expect(customer_attr).to have_received(:exact_evaluator).with(audience_3468206646_condition).once
+  end
+
+  it 'should correctly evaluate leaf node in audienceConditions' do
+    experiment = @project_typed_audience_instance.config.get_experiment_from_key('audience_combinations_experiment')
+    experiment['audienceConditions'] = '3468206645'
+    customer_attr = Optimizely::CustomAttributeConditionEvaluator.new({})
+
+    audience_3468206645 = @project_typed_audience_instance.config.get_audience_from_id('3468206645')
+    audience_3468206645_condition1 = audience_3468206645['conditions'][1][1][1]
+    audience_3468206645_condition2 = audience_3468206645['conditions'][1][1][2]
+    allow(customer_attr).to receive(:exact_evaluator)
+    customer_attr.evaluate(audience_3468206645_condition1)
+    customer_attr.evaluate(audience_3468206645_condition2)
+
+    expect(customer_attr).to have_received(:exact_evaluator).with(audience_3468206645_condition1).once
+    expect(customer_attr).to have_received(:exact_evaluator).with(audience_3468206645_condition2).once
+  end
 end
